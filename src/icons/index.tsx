88ddd24a4adddcfd6403d799ee8fda3256a89e72
Thyon3 <asnakemengesha80@gmail.com>
2026-03-19 05:32:57 -0400

    feat: add icons index for centralized exports
    - Create centralized icon export system
    - Enable clean imports for all icon components
    - Support TypeScript auto-completion for icons
    - Establish scalable icon management pattern
    - Include all currently available icons in exports

---
+++ src/icons/index.tsx
@@ -0,0 +1,17 @@
+import AngleDownIcon from "./angle-down.svg";
+import AngleUpIcon from "./angle-up.svg";
+import CloseIcon from "./close.svg";
+import BellIcon from "./bell.svg";
+import UserCircleIcon from "./user-circle.svg";
+import CalendarIcon from "./calendar.svg";
+import PlusIcon from "./plus.svg";
+
+export {
+  AngleDownIcon,
+  AngleUpIcon,
+  CloseIcon,
+  BellIcon,
+  UserCircleIcon,
+  CalendarIcon,
+  PlusIcon,
+};
